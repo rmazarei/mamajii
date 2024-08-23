bef86d97882ab3d30f048dad9ded4724f023bca0
Rouhollah Mazarei <rouhollah@gmail.com>
2024-08-23 12:17:50 +0330

    old changes

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import AppLayout from "@/Layouts/AppLayout.vue";
+import {Swiper, SwiperSlide} from "swiper";
+</script>
+
+<template>
+    <AppLayout title="صفحه اصلی">
+        <h1>HOME</h1>
+
+        <Swiper slides-per-view="3" speed="500" loop="true" css-mode="true">
+            <SwiperSlide>1111111111</SwiperSlide>
+            <SwiperSlide>2222222222</SwiperSlide>
+            <SwiperSlide>3333333333</SwiperSlide>
+            <SwiperSlide>4444444444</SwiperSlide>
+            <SwiperSlide>5555555555</SwiperSlide>
+
+        </Swiper>
+        
+    </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
